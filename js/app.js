cf0ddd42e2c5400aec015ceca0e71b029a5a466a
Kamil Kielan <kamilkielan@gmail.com>
2013-09-17 17:29:50 +0000

    Add more

--- js/app.js
+++ js/app.js
@@ -1,16 +1,44 @@
-function Master($scope) {
+(function (angular) {
+ var myAppModule = angular.module('myApp', []);
+
+myAppModule.directive('selectOnClick', function () {
+    // Linker function
+    return function (scope, element, attrs) {
+        element.bind('click', function () {
+        element[0].select();
+      });
+    };
+  });
+
+  // myAppModule.controller('Master', function ($scope) {
+  //   $scope.content = 'foobar';
+  // });
+myAppModule.controller('Master', function ($scope) {
 	// var toptext = 'client.send()';
-	var client = null;
+  $scope.photosQuantity = parseInt($scope.photosQuantity);
+  $scope.numberak = {};
+
+  $scope.calc = function() {
+    console.log(typeof +$scope.photosQuantity + "begin");
+    var i = $scope.photosQuantity;
+    while (i > 0) {
+      $scope.numberak[i] = i;
+      console.log($scope.numberak[i] + "insider");
+      i--;
+    };
+  };
+
+  var client = null;
 	var chai;
 	function getData() {
 		client = new XMLHttpRequest();
-		client.open('GET', 'text.txt');
+		client.open('GET', 'data/text.txt');
 		client.onreadystatechange = processRequest();
 		var insider;
 		client.send();
 		// chai = client.chail;
 		// return client.onreadystatechange
-	}
+	};
 
 	function processRequest() {
 		if (client.readyState==4 && client.status==200) {
@@ -21,7 +49,7 @@ function Master($scope) {
 			//this.blop = "aa" + client.responseText;
 			// alert(th.blop);
 		};
-	}
+	};
 	getData();
 
 
@@ -36,9 +64,10 @@ function Master($scope) {
 	$scope.payment = 'We must receive full payment both of the price of the Goods and any delivery charges before we can accept any offers. Where you select to pay by credit or debit card we will process the payment at the time of preparing your Goods for dispatch.';
 	$scope.shipping = 'All deliveries are subject to stock availability and authorisation of your payment. We aim to deliver your item within three working days using Standard Delivery. There are a small number of postcodes, usually in remote rural areas, where we cannot guarantee delivery within our usual timescale, however we will notify you as soon as possible. Delivery will only be made to your billing address. We deliver to most postal areas in the UK. We reserve the right not to deliver orders if we believe the address is not secure, for example, to a communal postal address or PO Box. If this affects your order, we will notify you as soon as possible. A signature will be required on receipt of your goods being delivered. If you are not around to accept your parcel a calling card will be posted through your door to let you know a delivery has been attempted. Please note that orders are only dispatched Monday to Friday. If an order is placed by 3:30pm on Friday, it will be dispatched the same day. If an order is placed after this time, or on Saturday and Sunday, the order will be dispatched on Monday. Please note that orders are not dispatched on public holidays.';
 	// $scope.result -
-}
+});
 
-function FetchCtrl($scope, $http, $templateCache) {
+myAppModule.controller('FetchCtrl', function ($scope, $http, $templateCache) {
+// function FetchCtrl($scope, $http, $templateCache) {
   $scope.method = 'GET';
   $scope.url = 'data/toptext.txt';
   $scope.numberos = null;
@@ -49,7 +78,6 @@ function FetchCtrl($scope, $http, $templateCache) {
     $scope.url = insideurl;
     $scope.data = {};
     $scope.data[dataproperty] = null;
-
 
     $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
       success(function(data, status) {
@@ -66,7 +94,8 @@ function FetchCtrl($scope, $http, $templateCache) {
     $scope.method = method;
     $scope.url = url;
   };
-}
+
+});
 
 // function SecondCtrl($scope, $http, $templateCache) {
 //   $scope.method = 'GET';
@@ -120,7 +149,9 @@ function FetchCtrl($scope, $http, $templateCache) {
 // // 	});
 // // }
 
-function ThirdCtrl($scope, $http) {
+
+myAppModule.controller('ThirdCtrl', function ($scope, $http) {
+// function ThirdCtrl($scope, $http) {
 	$scope.fetch = function($givenvar, $urlurl) {
     $http({method: 'GET', url: urlurl, cache: $templateCache}).
       success(function(data, status) {
@@ -141,4 +172,42 @@ function ThirdCtrl($scope, $http) {
 	// $q.all([$scope.topt, $scope.bottomt]).then(function(values) {
 	// 	$scope.results = MyService.doCalculation(values[0], values[1]);
 	// });
-}
+});
+
+myAppModule.controller('FourthCtrl', function ($scope, $http, $templateCache) {
+// function FourthCtrl($scope, $http, $templateCache) {
+  $scope.method = 'GET';
+  $scope.url = 'data/toptext.txt';
+  $scope.numberos = null;
+
+  $scope.fetch = function(insideurl, dataproperty) {
+    $scope.code = null;
+    $scope.response = null;
+    $scope.url = insideurl;
+    $scope.dataproperty = {};
+    $scope.dataproperty = dataproperty.split(",");
+    console.log($scope.dataproperty[0]);
+    $scope.data = {};
+    // $scope.data[dataproperty] = null;
+
+    $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+      success(function(data, status) {
+        $scope.dataprocessed = data.replace(/(\r\n|\n|\r)/gm,"").split("<!---------------------DELIMITER----------------->");
+        $scope.status = status;
+        for (element in $scope.dataproperty) {
+          console.log($scope.dataproperty[element]);
+          $scope.data[$scope.dataproperty[element]] = $scope.dataprocessed[element]; 
+        };
+      }).
+      error(function(data, status) {
+        $scope.data = data || "Request failed";
+        $scope.status = status;
+    });
+  };
+ 
+  $scope.updateModel = function(method, url) {
+    $scope.method = method;
+    $scope.url = url;
+  };
+});
+}(angular)); 
